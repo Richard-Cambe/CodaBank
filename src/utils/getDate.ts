fb51a9aa660bdccc32c1db4d8065b1df6a708c3a
Richard-Cambe <richard.cambe@outlook.fr>
2025-01-08 12:16:40 +0100

    fix date

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -3,6 +3,5 @@ export function getDate():string {
     const month :number = today.getMonth() + 1;
     const year :number = today.getFullYear();
     const date :number = today.getDate();
-    console.log(date);
     return `${month}/${date}/${year}`;
 }
