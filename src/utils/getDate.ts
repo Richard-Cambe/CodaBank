798e0c157f93c75604895985b33f1591bb54aaae
Richard-Cambe <richard.cambe@outlook.fr>
2025-01-08 12:16:18 +0100

    added date + fix color messages

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -3,5 +3,6 @@ export function getDate():string {
     const month :number = today.getMonth() + 1;
     const year :number = today.getFullYear();
     const date :number = today.getDate();
+    console.log(date);
     return `${month}/${date}/${year}`;
 }
